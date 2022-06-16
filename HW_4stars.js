78b10947c310a8c0d9aac15a04a124b3648474f5
ElenaYushkevich <millenchick@gmail.com>
2022-06-16 10:24:27 +0300

    edit changes into file 4stars

--- HW_4stars.js
+++ HW_4stars.js
@@ -5,20 +5,22 @@ const age_2 = prompt('enter second age')
 const age_3 = prompt('enter third age')
 
 const checkAge = function(age_1, age_2, age_3) {
-        if (!isNaN(Number(arguments[0])) && !isNaN(Number(arguments[1])) && !isNaN(Number(arguments[2]))) { 
-        
-          if (age_1 < age_2) {
-                  alert(`You don t have access cause your age is ${age_1}, It’s less then ${age_2}`)
-          } 
-          else if (age_1 >= age_2 && age_1 < age_3) {
-                   alert("Welcome  !")
-          }
-          else if (age_1  > age_3) {
-                   alert("Keep calm and look Culture channel")
-          }
-                }
-          else {
-                   alert("Technical work")
-          }
-       }
-     checkAge(age_1, age_2, age_3)
+  if (isValid(arguments[0]) && isValid(arguments[1]) && isValid(arguments[2])) { 
+    if (age_1 < age_2) {
+        alert(`You don t have access cause your age is ${age_1}, It’s less then ${age_2}`)
+    } 
+    else if (age_1 >= age_2 && age_1 < age_3) {
+        alert("Welcome  !")
+    }
+    else if (age_1  >= age_3) {
+        alert("Keep calm and look Culture channel")
+    }
+  }
+  else {
+    alert("Technical work")
+  }
+}
+function isValid(value) {
+   return value && !isNaN(value) && typeof(value) !== "boolean"
+}  
+checkAge(age_1, age_2, age_3)
