a6122bac28871db33d7e3b5487d62670c12a3101
ElenaYushkevich <millenchick@gmail.com>
2022-06-16 10:23:34 +0300

    edit changes into file 3stars

--- HW_3stars.js
+++ HW_3stars.js
@@ -2,22 +2,24 @@
 //(строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
 
 const checkAge = function(age_1, age_2, age_3) {
-//    if ((typeof Number(arguments[0]) === "number" && !isNaN(Number(arguments[0]))) && (typeof Number(arguments[1]) === "number" && !isNaN(Number(arguments[1]))) && (typeof Number(arguments[2]) === "number" && !isNaN(Number(arguments[2])))) { 
-    if (!isNaN(Number(arguments[0])) && !isNaN(Number(arguments[1])) && !isNaN(Number(arguments[2]))) { 
-    
+    if (isValid(arguments[0]) && isValid(arguments[1]) && isValid(arguments[2])) { 
       if (age_1 < age_2) {
-              console.log("You don’t have access cause your age is", age_1, "It’s less then", age_2)
+        console.log("You don’t have access cause your age is", age_1, "It’s less then", age_2)
       } 
       else if (age_1 >= age_2 && age_1 < age_3) {
-               console.log("Welcome  !")
+        console.log("Welcome  !")
       }
-      else if (age_1  > age_3) {
-               console.log("Keep calm and look Culture channel")
+      else if (age_1  >= age_3) {
+        console.log("Keep calm and look Culture channel")
       }
             }
       else {
-               console.log("Technical work")
+        console.log("Technical work")
       }
    }
- checkAge(17, 18, 61)
- 
+function isValid(value) {
+  return value && !isNaN(value) && typeof(value) !== "boolean"
+}  
+checkAge(17, 18, 61)
+checkAge(true, 18, 61)
+checkAge("SS", 18, 61)
